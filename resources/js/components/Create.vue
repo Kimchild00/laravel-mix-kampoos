0291ff3103bd2a840ca89efc4d107a6d93014761
Kimchild00 <habib.barokah91@gmail.com>
2021-09-10 11:21:51 +0700

    ok

---
+++ resources/js/components/Create.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header text-center">Tambah buku</div>
+
+                    <div class="card-body">
+                        <form action="">
+                            <div class="form-group">
+                                <input type="text" name="name" id="" placeholder="Masukan Nama" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" name="title" id="" placeholder="Masukan Nama" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <textarea name="description" class="form-control" id="" rows="5" holder="Description"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <router-link to="/" class="btn btn-warning">Kembali</router-link>
+                                <button class="btn btn-success">Simpan</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
---
+++ resources/js/components/Index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            <router-link to="/create" class="btn btn-primary float-right">Tambah buku </router-link>
+        </div>
+
+        <div class="card-body">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nama</th>
+                        <th>title</th>
+                        <th>desc</th>
+                        <th>action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>habib</td>
+                        <td>kepbo</td>
+                        <td>isi kepbo</td>
+                        <td>edit</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
